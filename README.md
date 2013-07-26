8ccd937edf4241ac1d47b51155a934c572b24be9
Moshe Kolodny <kolodny@github.com>
2013-07-25 23:00:08 -0400

    inital commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-zerocomplete
-============
+## Zerocomplete
+
+---
+
+### [DEMO](http://goo.gl/9Rs6VB)
+
+---
+
+#### Usage:
+`$('input').zerocomplete(options)`
+
+Here's a quick rundown of what options can contain:
+
+Name          |   type           |     default       |   description
+:---:         | ---              | :---:             | ---
+`source`      | array, string    |   [ ]             | The data source to query against. If this is a string then it's assumed to be a url to send the query to in the `term` get param. If it's an array, it can be an array of strings or an array of objects.
+`delay`       | number           | `300`             | The amount of time to wait after the keypress before the ajax call
+`display`     | function, string | `label`           | When choosing from an array of objects, we need to know which property of the object to display in the dropdown (You don't want to display `[object Object]` for each item)
+`select`      | function         | (nothing)         | The callback that gets called when the user selects an item from the dropdown or blurs the input when they don't choose an item from the list. You can also bind to the element's `zerocompleteselect` event
+`getPath`     | function         | `return res.data` | When using ajax as the source, this is the callback that will take the response object and get the source data, defaults to: `function(res) { return res.data || res; }`
+`get`         | function         | `jQuery.get`      | This is the function that does the ajax call, you can override it to send a xsrf token or to do some other logic, the term is the context and it passes the url and callback to run with the response passed in. eg <br> `function (url, cb) {`<br> `  $.post(url, {some: 'data'}, function(res) { cb(res); });` <br>  `}`
+`response`    | function         | (nothing)         | This function is called after an ajax request completes but before the items are rendered, this gets passed the array and should return an array
+`highlighter` | function         | typeahead func    | You can override the default behavior or way the the matched text is bolded. Useful for displaying complex views
+`cache`       | object, false    | jQuery.cache.ajax | By default all ajax calls are cached to `jQuery.cache.ajax`, you can set this to another object, or turn it off by setting it to false| &nbsp;
+`search`      | function         | (nothing)         | Callback that gets fired before a dropdown is displayed, return false to cancel| &nbsp;
+
+Note that all standard typeahead options (except `source`) get passed in with the typeahead invocation so all typeahead options should also work
---
+++ zerocomplete.js
@@ -0,0 +1,162 @@
+(function($) {
+	var pluginName = 'zerocomplete',
+		events = ['select', 'open', 'search'];
+		
+	$.fn[pluginName] = function(options) {
+		var $wrappedSet = this,
+			source = [],
+			copyOfOptions = $.extend({}, options),
+			cleanItem = function(item) { return !isObjects ? item : item.replace(/^<!-- \d+ -->/, ''); },
+			getObjFromRawItem = function(item) { return !isObjects ? item : arrayOfObjects[item.match(/^<!-- (\d+) -->/)[1]]; },
+			usingAjax,
+			pendingUrl,
+			cache = options.cache || $.cache.ajax,
+			isObjects,
+			arrayOfObjects,
+			lastSelectedItem,			
+			proccessStringsOrObjects = function(something, cachedSource) {
+				if ($.isPlainObject(something[0])) {
+					if (cachedSource) {
+						source = cachedSource;
+					} else {
+						source = [];
+						for (var i = 0; i < something.length; i++) {
+							source.push( '<!-- ' + i + ' -->' + ($.isFunction(options.display) ? options.display(something[i], something) : something[i][options.display]) );
+						}
+					}
+					arrayOfObjects = something;
+					isObjects = true;
+				} else {
+					source = something;
+					arrayOfObjects = null;
+					isObjects = false;
+				}
+			};
+			
+		delete copyOfOptions.source;
+
+		options = $.extend({
+			display: 'label',
+			getPath: function(res) { return res.data || res; },
+			delay: 300
+		}, options);
+		
+		
+		if (typeof options.source === 'string') {
+			if (/^[^[]/.test(options.source)) {
+				usingAjax = true;
+			} else {
+				options.source = JSON.parse(options.source);
+				proccessStringsOrObjects(options.source);
+			}
+		} else {
+			proccessStringsOrObjects(options.source);
+		}
+		
+		$.each(events, function(i, event) {
+			if (options[event]) {
+				$wrappedSet.on(pluginName + event, function() { options[event].apply($wrappedSet, arguments); });
+			}
+		});
+
+		return this.each(function() {
+			var $this = $(this),
+				debouncedGet;
+			
+			$this.on('input', function() {
+				lastSelectedItem = undefined;
+				$this.off('blur.' + pluginName).one('blur.' + pluginName, function() {
+					if (!lastSelectedItem && !$this.data().typeahead.shown) {
+						$this.trigger(pluginName + 'select', { item: lastSelectedItem });
+					}
+				});
+			});
+			
+			$this.typeahead($.extend(copyOfOptions, {
+				source: function(query, process) {
+					var processWapper = function(source) {
+						if ($this.triggerHandler(pluginName + 'search', { query : query }) !== false) {
+							process(source);
+							$this.trigger(pluginName + 'open', { query : query });
+						}
+					};
+					
+					if (!usingAjax) {
+						processWapper(source);
+					} else {
+						var url = options.source + (options.source.indexOf('?') === -1 ? '?' : '&') + 'term=' + query;
+						
+						if (!cache || !cache[url]) {							
+							pendingUrl = url;
+							if (!debouncedGet) {
+								debouncedGet = $.fn[pluginName].debounce(options.get || $.get, options.delay)
+							}
+							debouncedGet.call(query, url, function(res) {
+								var something;
+								
+								if (cache) {
+									cache[url] = { res: res };
+								}
+								if (pendingUrl === url) {
+									something = options.getPath(res);
+									if (options.response) {
+										something = options.response(something)
+									}
+									proccessStringsOrObjects(something);
+									if (cache) {
+										cache[url] = { res: res, source: source, arrayOfObjects: arrayOfObjects };
+									}
+									processWapper(source);
+								}
+							});
+						} else {
+							pendingUrl = false;
+							proccessStringsOrObjects( options.getPath( cache[url].res ) );
+							if (cache && !cache[url].source) {
+								cache[url] = { res: cache[url].res, source: source, arrayOfObjects: arrayOfObjects };
+							}
+							processWapper(source);
+						}
+					}
+				},
+				updater: function(item) {
+					$this.data(pluginName)
+					lastSelectedItem = getObjFromRawItem(item);
+					$this.trigger(pluginName + 'select', { item: lastSelectedItem });
+					return cleanItem(item);
+				},
+				highlighter: function(item) {
+					if (options.highlighter) {
+						return options.highlighter.call(this, cleanItem(item), getObjFromRawItem(item), arrayOfObjects);
+					} else {
+						return $.fn.typeahead.Constructor.prototype.highlighter.call(this, cleanItem(item));						
+					}
+				},
+				matcher: function(item) {
+					return $.fn.typeahead.Constructor.prototype.matcher.call(this, cleanItem(item));
+				}
+			}));
+		});
+	};
+	
+	$.cache.ajax = {};
+	
+	$(document).on('focus.zerocomplete.data-api', '[data-provide="zerocomplete"]', function(e) {
+		var $this = $(this);
+		
+		if ($this.data('zerocomplete')) { return }
+		$this.zerocomplete($this.data());
+		$this.data('zerocomplete', true);
+	});
+	
+	$.fn[pluginName].debounce = function(fn, ms) {
+		var timer;
+		
+		return function() {
+			var ctx = this, args = arguments;
+			
+			clearTimeout(timer);
+			timer = setTimeout(function() { fn.apply(ctx, args); }, ms);
+		}
+	}
+})(jQuery);
